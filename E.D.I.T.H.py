b6a507bf3f312ff5679f95c9ee2d01b5111a0308
Nobita124 <88026515+Nobita124@users.noreply.github.com>
2022-06-06 08:16:56 -0700

    Update E.D.I.T.H.py

--- E.D.I.T.H.py
+++ E.D.I.T.H.py
@@ -1,132 +1,161 @@
-import pyautogui
+#pip install commands should be ran on cmd/command prompt
+import pyautogui #pip install pyautogui
 from time import sleep
-import pyttsx3
-import speech_recognition as sr
-import pyaudio
+import pyttsx3 #pip install pyttsx3
 import os
-import webbrowser
+import webbrowser #pip install webbrowser
 from selenium import webdriver #pip install InstaPy
 from webdriver_manager.chrome import ChromeDriverManager #pip install webdriver_manager
 from getpass import getpass
 from random import randint
 from Greet import greet
+from Help import help
 engine = pyttsx3.init('sapi5')
 voices = engine.getProperty('voices')
 engine.setProperty('voice', voices[2].id)
-#for male voice type "voices[0].id" for female voice type "voices[1].id"
+#for male voice type "voices[0].id" for female voice type "voices[1].id only if you have only two voices"
+#I have three voices so I use voices[2].id for female voice i.e Zira
 
 
-##This is main Code
+#This is main Code
 
 def speak(audio):
     engine.say(audio)
     engine.runAndWait()
 
+greet()
+
 def start():
-    greet()
-    speak('Please tell me what can I do for you ...')
-    com = input()
-    com = com.lower()
-    coma = com.split(" ")
-    if coma[0].startswith('open'):
-        speak(coma[0] + "ing " + coma[1])
-        sleep(0.50)
-        pyautogui.hotkey('win','r')
-        sleep(2)
-        pyautogui.write(coma[1])
-        pyautogui.press('enter')
-
-    elif 'play music' in com : 
-        music_dir = 'Song'
-        song = os.listdir(music_dir)
-        print(song)
-        speak('Sir! please Copy and paste the name of the music from above you wanna play')
-        name = input()
-        speak('playing music' + name)
-        sleep(0.50)
-        os.startfile(os.path.join(music_dir, name))
-    elif coma[0].startswith('shut'):
-        speak(coma[0] + 'ing ' + coma[1])
-        sleep(0.50)
-        pyautogui.hotkey('win','r')
-        sleep(0.75)
-        pyautogui.write('shutdown -s')
-        sleep(1)
-        pyautogui.press('enter')
-    elif coma[0].startswith('off'):
-        speak(coma[0] + 'ing ' + coma[1])
-        sleep(0.50)
-        pyautogui.hotkey('alt','tab')
-        sleep(2)
-        pyautogui.hotkey('alt','f4')
-    elif coma[0].startswith('clean'):
-        speak(coma[0] + 'ing ' + coma[1])
-        sleep(0.50)
-        pyautogui.hotkey('win','r')
-        sleep(1)
-        pyautogui.write(coma[1])
-        pyautogui.press('enter')
-        sleep(1.5)
-        #pyautogui.press('enter')
-        sleep(0.25)
-        pyautogui.hotkey('ctrl','a')
-        sleep(0.50)
-        pyautogui.hotkey('shift','del')
-        sleep(1)
-        pyautogui.press('enter')
-    elif coma[0].startswith('defragment'):
-        speak(coma[0] + 'ing ')
-        sleep(0.50)
-        pyautogui.press('win')
-        pyautogui.write('Defragmentation')
-        sleep(2.5)
-        pyautogui.moveTo(243,119,duration=0.25)
-        pyautogui.click()
-    elif 'play movie' in com :
-        movie_dir = 'D:\\Movies'
-        film = os.listdir(movie_dir)
-        print(film)
-        speak('Copy and paste the name of the movie from above you wanna play')
-        nam = input()
-        speak('playing movie' + nam)
-        sleep(0.50)
-        os.startfile(os.path.join(movie_dir, nam))
-    elif 'youtube' in com :
-        driver = webdriver.Chrome(ChromeDriverManager().install())
-        driver.get('https://www.youtube.com/')
-    elif 'google' in com :
-        webbrowser.open("google.com")
-    elif 'log in on insta' in com:
-        nme = input("Enter your username : ")
-        passwor= getpass('Enter your password : ')
-        driver = webdriver.Chrome(ChromeDriverManager().install())
-        driver.get('https://www.instagram.com/')
-        print ("Opened Instagram")
-
-        sleep(randint(1,4))
-        username = driver.find_element_by_name('username')
-        username.send_keys(nme)
-        password = driver.find_element_by_name('password')
-        password.send_keys(passwor)
     
-        button_login = driver.find_element_by_css_selector('#loginForm > div > div:nth-child(3) > button')
-        button_login.click()
-        sleep(randint(3,5))
+        order = '\nPlease tell me what can I do for you ...\n'
+        print(order)
+        speak(order)
+        com = input('--> ')
+        com = com.lower()
+        coma = com.split(" ")
+        if coma[0].startswith('open'):
+            task = coma[0] + "ing " + coma[1] + '\n'
+            print(task)
+            speak(task)
+            sleep(randint(1,5))
+            pyautogui.hotkey('win','r')
+            sleep(randint(1,5))
+            pyautogui.write(coma[1])
+            pyautogui.press('enter')
 
-        notnow = driver.find_element_by_css_selector('#react-root > section > main > div > div > div > div > button')
-        notnow.click()
-        sleep(randint(3,5))
-        notificationnotnow = driver.find_element_by_xpath('/html/body/div[4]/div/div/div/div[3]/button[2]')
-        notificationnotnow.click()
+        elif 'play music' in com : 
+            music_dir = 'Song' #your music directory
+            song = os.listdir(music_dir)
+            print(song)
+            task = '\nSir! please Copy and paste the name of the music from above you wanna play' + '\n'
+            print(task)
+            speak(task)
+            name = input()
+            na = '\nplaying music ' + name
+            print(na)
+            speak(na)
+            sleep(randint(1,5))
+            os.startfile(os.path.join(music_dir, name))
+        elif coma[0].startswith('shut'):
+            task = coma[0] + 'ing ' + coma[1] + '\n'
+            print(task)
+            speak(task)
+            sleep(randint(1,5))
+            pyautogui.hotkey('win','r')
+            sleep(randint(1,5))
+            pyautogui.write('shutdown -s')
+            sleep(randint(1, 5))
+            pyautogui.press('enter')
+        elif coma[0].startswith('stop'):
+            task = coma[0] + 'ing ' + coma[1] + '\n'
+            print(task)
+            speak(task)
+            sleep(randint(1,5))
+            pyautogui.hotkey('alt','tab')
+            sleep(randint(1,5))
+            pyautogui.hotkey('alt','f4')
+        elif coma[0].startswith('clean'):
+            task = coma[0] + 'ing ' + coma[1] + '\n'
+            print(task)
+            speak(task)
+            sleep(randint(1,5))
+            pyautogui.hotkey('win','r')
+            sleep(randint(1,5))
+            pyautogui.write(coma[1])
+            pyautogui.press('enter')
+            sleep(randint(1, 5))
+            #pyautogui.press('enter')
+            sleep(randint(1,5))
+            pyautogui.hotkey('ctrl','a')
+            sleep(randint(1,5))
+            pyautogui.hotkey('shift','del')
+            sleep(randint(1,5))
+            pyautogui.press('enter')
+        
+        elif 'play movie' in com :
+            movie_dir = 'D:\\Movies' #your movie directory
+            film = os.listdir(movie_dir) 
+            print(film)
+            task = 'Copy and paste the name of the movie from above you wanna play'+ '\n'
+            print(task)
+            speak(task)
+            nam = input()
+            flm = '\nplaying movie ' + nam
+            print(flm)
+            speak(flm)
+            sleep(randint(1,5))
+            os.startfile(os.path.join(movie_dir, nam))
+        elif 'youtube' in com :
+            driver = webdriver.Chrome(ChromeDriverManager().install())
+            driver.get('https://www.youtube.com/')
+            task = 'opening youtube \n'
+            print(task)
+            speak(task)
+        elif 'google' in com :
+            webbrowser.open("google.com")
+        elif 'log in to insta' in com:
+            nme = input("Enter your username : ")
+            passwor= getpass('Enter your password : ')
+            driver = webdriver.Chrome(ChromeDriverManager().install())
+            driver.get('https://www.instagram.com/')
+            print ("Opened Instagram \n")
+
+            sleep(randint(1,4))
+            username = driver.find_element_by_name('username')
+            username.send_keys(nme)
+            password = driver.find_element_by_name('password')
+            password.send_keys(passwor)
+    
+            button_login = driver.find_element_by_css_selector('#loginForm > div > div:nth-child(3) > button')
+            button_login.click()
+        #sleep(randint(3,5))
+
+        #notnow = driver.find_element_by_css_selector('#react-root > section > main > div > div > div > div > button')
+        #notnow.click()
+        #sleep(randint(3,5))
+        #notificationnotnow = driver.find_element_by_xpath('/html/body/div[4]/div/div/div/div[3]/button[2]')
+        #notificationnotnow.click()
+        elif 'help' in com :
+            help()
+            return start()
+        else :
+            non = 'Sorry Sir, Command Not found'
+            print(non)
+            speak(non)
+            return start()
 
 if __name__ == "__main__":
     start()
     while True:
-         speak('Is there anything else I can do for you : ')
-         con=input()
+        
+         end = ('\nIs there anything else I can do for you : \n')
+         print(end)
+         speak(end)
+         con=input('-->')
          if con.startswith('Y'):
                 start()
          elif con.startswith('N'):
-                speak('Bye Sir ! I Hope that we will meet again ')
+                con = '\nBye Sir ! I Hope that we will meet again \n'
+                print(con)
+                speak(con)
                 exit()
-               
